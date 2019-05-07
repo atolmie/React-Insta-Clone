4fc460fd0b89070e2c601320d30d798e64c72e2e
atolmie <atolmie22@gmail.com>
2019-05-07 16:52:48 -0500

    searchbar structure

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -3,15 +3,21 @@ import './App.css';
 import PropTypes from 'prop-types';
 import dummyData from './dummy-data';
 import PostContainer from './components/PostContainer/PostContainer';
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
   return (
     <div className="App">
+      <SearchBar />
       {dummyData.map(post => {
         return <PostContainer key={post.id} post={post} />
       })}
     </div>
   );
+}
+
+componentDidMount() {
+
 }
 
 export default App;
---
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './SearchBar.css';
+
+
+function SearchBar() {
+    return (
+        <div className="search-bar">
+            <h1>Instagram</h1>
+            <input type="text" />
+            <div>icons</div>
+        </div>
+    );
+}
+
+export default SearchBar;
